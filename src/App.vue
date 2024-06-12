c83a3ea4fbd790c61d316ec5d1c83d809f8394f5
richu <1691401076@qq.com>
2024-06-12 15:54:47 +0800

    #6.13

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
 </template>
 
 <script setup lang="ts">
-import { Delete } from '@element-plus/icons-vue'
 
 </script>
 
--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
 export default {
     title:'欢迎',
-    logo:'../'
+    logo:'../../public/favicon.ico'
 }
