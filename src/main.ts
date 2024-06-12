19eb514623b7dea314af4670dba9ef1866e653a3
richu <1691401076@qq.com>
2024-06-12 14:25:06 +0800

    #入口文件

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
+/* 使用pinia仓库 */
+import store from './stores/index'
+/* 引入ElementPlus */
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+/* 引入路由模块 */
+import Route from './router'
+/* import zhCn from 'element-plus/dist/locale/zh-cn.min.mjs' */
 
 const app = createApp(App)
+app.use(store)
+app.use(Route)
+app.use(ElementPlus)
 
-app.use(createPinia())
+//国际化，使用中文
+/* app.use(ElementPlus,{
+    locale:zhCn
+}) */
 
 app.mount('#app')
