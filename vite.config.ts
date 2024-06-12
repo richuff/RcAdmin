55431f3896820cf3793b448141c43942c12048ef
richu <1691401076@qq.com>
2024-06-12 14:23:22 +0800

    #config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import {viteMockServe} from "vite-plugin-mock"
+import { defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
+import path from 'path'
+
+export default defineConfig(({ command }) => {
+  return {
   plugins: [
     vue(),
+    viteMockServe({
+      mockPath:'mock',
+      localEnabled: command === "serve",
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          hack: `true; @import (reference) "${path.resolve("src/styles/base.less")}";`,
+        },
+        javascriptEnabled: true,
+      },
+    },
   }
-})
+}}
+)
