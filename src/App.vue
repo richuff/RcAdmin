bf675c90b43fe165877c41320076a7dc96bdb3b1
richu <1691401076@qq.com>
2024-06-12 14:22:34 +0800

    基本配置#6.12

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+</template>
+
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+  export default{
+    name:'App',
+    components:{HelloWorld}
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    props:['msg'],
+    setup(){
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
