07252485ec21613a14ad86ab266c0ff39b34b212
richu <1691401076@qq.com>
2024-08-24 11:16:02 +0800

    #chore:添加详细说明

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-<h2>richu_vueapp</h2><br/>
+<h2>richu_vueapp</h2>
 This a project writed by Vue3 and typescript 
 
 <h2>Quick Start</h2>
->Bulid with Vite <br/>
->1.npm i pnpm <br/>
->2.pnpm install <br/>
->3.pnpm run start
+
+> Bulid with Vite <br/>
+> 1.npm i pnpm <br/>
+> 2.pnpm install <br/>
+> 3.pnpm run start
--- src/permission.ts
+++ src/permission.ts
@@ -1,8 +1,12 @@
+//引入路由模块
 import router from './router/index'
 //@ts-ignore
+//引入nprogress和css文件
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css';
+//引入仓库模块
 import userStore from './stores/modules/user';
+//路由前鉴权
 router.beforeEach(async (to: any, from: any, next: any) => {
     document.title = "VT-" + to.meta.title;
     let UserStore = userStore();
@@ -34,6 +38,7 @@ router.beforeEach(async (to: any, from: any, next: any) => {
     }
 })
 
+//路由后鉴权，关闭进度条
 router.afterEach((to: any, from: any) => {
     NProgress.done();
 })
