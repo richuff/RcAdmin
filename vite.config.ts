246014e8307708aa8df7f526166d9cf9b2bb74a2
richu <1691401076@qq.com>
2024-08-25 19:17:36 +0800

    #perf:文件导入路径优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,51 @@
 import { fileURLToPath, URL } from 'node:url'
-import {viteMockServe} from "vite-plugin-mock"
-import { defineConfig} from 'vite'
+import { viteMockServe } from "vite-plugin-mock"
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import path from 'path'
-import {createSvgIconsPlugin} from 'vite-plugin-svg-icons'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 export default defineConfig(({ command }) => {
   return {
-  plugins: [
-    vue(),
-    //svg
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(),'src/assets/icons')],
-      symbolId:'icon-[dir]-[name]',
-    }),
-    viteMockServe({
-      mockPath:'mock',
-      localEnabled: command === "serve",
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-
-  css: {
-    preprocessorOptions: {
-      less: {
-        modifyVars: {
-          hack: `true; @import (reference) "${path.resolve("src/styles/base.less")}";`,
+    plugins: [
+      vue(),
+      //svg
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]',
+      }),
+      viteMockServe({
+        mockPath: 'mock',
+        localEnabled: command === "serve",
+      }),
+    ],
+    resolve: {
+      alias: {
+        //'@': fileURLToPath(new URL('./src', import.meta.url))
+        "@": path.resolve(__dirname, "./src")
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          modifyVars: {
+            hack: `true; @import (reference) "${path.resolve("src/styles/base.less")}";`,
+          },
+          javascriptEnabled: true,
         },
-        javascriptEnabled: true,
       },
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://sph-api.atguigu.cn',
-        /* 是否代理跨域 */
-        changeOrigin: true,
-        /* 是否路径重写 */
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    }}
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://sph-api.atguigu.cn',
+          /* 是否代理跨域 */
+          changeOrigin: true,
+          /* 是否路径重写 */
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      }
+    }
   }
 }
 )
