782f52c3874bf890f123e45fa0f88eeaa2b70c6c
richuff <1691401076@qq.com>
2025-06-06 10:21:50 +0800

    add attr api

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -7,11 +7,6 @@ enum API {
     USER_LOGOUT = '/api/admin/acl/index/logout'
 }
 
-<<<<<<< HEAD
-export const UserLogin=(data:Iuserinfo)=>request.post(API.USER_LOGIN,data);
-export const UserInfo=()=>request.get(API.USER_INFO);
-=======
 export const UserLogin = (data: Iuserinfo) => request.post<any, any>(API.USER_LOGIN, data);
 export const UserInfo = () => request.get<any, any>(API.USER_INFO);
 export const UserLoginout = () => request.get<any, any>(API.USER_LOGOUT);
->>>>>>> 56a8778e9e5317507b7d344cda36908b812a3b4c
